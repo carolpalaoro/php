d745ffb8d346638e7f38769d1a888a54a14035fc
carolpalaoro <carolpalaoro.senai@gmail.com>
2023-08-10 14:31:42 -0300

    phpaula1
    Aula 1 -  Sintaxe e servidor local com XAMPP

---
+++ Aula01/sintaxe3.php
@@ -0,0 +1,15 @@
+<?php
+
+echo                               "testando espaço em branco <br>";
+echo "testando espaço <br>"             ;
+
+
+// o caracter &nbsp; adiciona um espaço em branco dentro da string
+echo "&#160; &#160;                                testando        espaço      em        branco <br>";
+"quebra de linha <br>"
+;
+
+
+echo "quebra de linha <br>"
+;
+?>
